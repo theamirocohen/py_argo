573249da1a1e0d45ab25a3da2a1dbfb9925ffa36
Ubuntu <ubuntu@ip-172-31-4-177.eu-north-1.compute.internal>
2025-06-07 17:23:10 +0000

    change docker version for argo to sync

--- py_app/app.py
+++ py_app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Prometheus Monitoring Exercise!"
+    return "Hello,  ARGOCD Exercise!"
 
 @app.route("/metrics")
 def metrics():
